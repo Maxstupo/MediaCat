147e7ffe68bb104a3aa5c92650e6a4f89fea153e
Maxstupo <maxstupo@hotmail.com>
2021-09-13 16:14:50 +1000

    support building without appveyor

--- appveyor-build.ps1
+++ appveyor-build.ps1
@@ -1,23 +1,44 @@
-$env:Path += ";C:\Program Files (x86)\Inno Setup 6"
+param ($config='Release')
 
-$version = $env:APPVEYOR_BUILD_VERSION
+$env:Path += ";C:\Program Files (x86)\Inno Setup 6;C:\Program Files\7-Zip"
+$appversion = $env:APPVEYOR_BUILD_VERSION
+
+# Setup script if running outside of AppVeyor. Use 0.0.0.0 for version since msbuild doesn't update the version anyway
+if (!$env:APPVEYOR) {
+    $appversion = "0.0.0.0"
+    $env:CONFIGURATION = $config
+    
+    Write-Host "################# Restoring Dependencies #################"  
+    msbuild MediaCat.sln /t:Restore
+   
+    Write-Host "################# Building #################"  
+    msbuild MediaCat.sln /P:Configuration=$config
+}
 
 # When building from tag, use only "major.minor.patch" else use "major.minor.patch.build"
-if ($env:APPVEYOR_REPO_TAG -eq $true) {
-    $version = $env:BUILD_VERSION
+if ($env:APPVEYOR_REPO_TAG -eq "true") {
+    $appversion = $env:BUILD_VERSION
 }
-    
-$portable_folder = "MediaCat-Portable_v$version"
+
+# Copy and archive all files needed for portable version 
+Write-Host "################# Packaging Portable Edition #################"   
+$portable_folder = "MediaCat-Portable_v$appversion"
 
 New-Item -Path $portable_folder -ItemType Directory
-
-# Copy all files needed for portable version
-Write-Host "Building portable distro"
 Copy-Item "MediaCat/bin/$env:CONFIGURATION/*" $portable_folder -Force -Recurse -Verbose
 
 # Make the portable version
 7z a "$portable_folder.zip" "$portable_folder"
 
+# Remove temp folder
+Get-ChildItem -Path $portable_folder -Recurse | Remove-Item -Force -Recurse
+Remove-Item $portable_folder -Force
+
+
 # Make the installer
-Write-Host "Building installer distro"
-iscc mediacat-installer.iss /DConfiguration=$env:CONFIGURATION /DApplicationVersion=$version
+Write-Host "################# Building Installer #################"
+iscc mediacat-installer.iss /DConfiguration=$env:CONFIGURATION /DApplicationVersion=$appversion
+
+
+# Pause if running outside of AppVeyor
+if (!$env:APPVEYOR) { pause }
