fa47979b6d52c777c287e7e9dce360a926ff9bb4
Maxstupo <maxstupo@hotmail.com>
2021-09-18 03:01:40 +1000

    make generic

--- appveyor-build.ps1
+++ appveyor-build.ps1
@@ -1,4 +1,4 @@
-param ($config='Release')
+param($config='Release',$build='true',$version='0.0.0.0',$title='MediaCat',$project_name='MediaCat',$iss='mediacat-installer.iss')
 
 $env:Path += ";C:\Program Files (x86)\Inno Setup 6;C:\Program Files\7-Zip"
 
@@ -6,24 +6,30 @@ $appversion = $env:APP_VERSION
 
 # Setup script if running outside of AppVeyor. Use 0.0.0.0 for version since msbuild doesn't update the version anyway
 if (!$env:APPVEYOR) {
-    $appversion = "0.0.0.0"
+    $env:TITLE = $title
+    $env:PROJECT_NAME = $project_name
+    $env:ISS = $iss
+
+    $appversion = $version
     $env:CONFIGURATION = $config
     
-    Write-Host "################# Restoring Dependencies #################"  
-    msbuild MediaCat.sln /t:Restore
+    if($build -eq 'true') {
+        Write-Host "################# Restoring Dependencies #################"  
+        msbuild "$env:PROJECT_NAME.sln" /t:Restore
    
-    Write-Host "################# Building #################"  
-    msbuild MediaCat.sln /P:Configuration=$config
+        Write-Host "################# Building #################"  
+        msbuild "$env:PROJECT_NAME.sln" /P:Configuration=$config
+    }
 }
 
 
 
 # Copy and archive all files needed for portable version 
 Write-Host "################# Packaging Portable Edition #################"   
-$portable_folder = "MediaCat-Portable_v$appversion"
+$portable_folder = "$env:TITLE-Portable_v$appversion"
 
 New-Item -Path $portable_folder -ItemType Directory
-Copy-Item -Exclude '*.pdb', '*.sqlite3' "MediaCat/bin/$env:CONFIGURATION/*" $portable_folder -Force -Recurse -Verbose
+Copy-Item -Exclude '*.pdb', '*.sqlite3' "$env:PROJECT_NAME/bin/$env:CONFIGURATION/*" $portable_folder -Force -Recurse -Verbose
 
 # Make the portable version
 7z a "$portable_folder.zip" "$portable_folder"
@@ -35,7 +41,7 @@ Remove-Item $portable_folder -Force
 
 # Make the installer
 Write-Host "################# Building Installer #################"
-iscc mediacat-installer.iss /DConfiguration=$env:CONFIGURATION /DApplicationVersion=$appversion
+iscc $env:ISS /DConfiguration=$env:CONFIGURATION /DApplicationVersion=$appversion
 
 
 # Pause if running outside of AppVeyor
