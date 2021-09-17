d675ae8e7bb7aae942e107b2697c94dc8c05c2c4
Maxstupo <maxstupo@hotmail.com>
2021-09-18 02:12:32 +1000

    support branches

--- MediaCat/Properties/AssemblyInfo.cs
+++ MediaCat/Properties/AssemblyInfo.cs
@@ -53,3 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
